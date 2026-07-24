85ea989de5ac0136f2c267c991781f90bcd9ea98
Misaki Ae-rin <izkalavender@gmail.com>
2026-07-24 23:59:07 +0700

    Update background.js

--- background.js
+++ background.js
@@ -1,10 +1,10 @@
-// background.js
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.action === 'get_stockfish') {
-    fetch(chrome.runtime.getURL('stockfish.js'))
+    // Mengambil Stockfish langsung dari internet (Bypass lokal & CSP)
+    fetch('https://cdnjs.cloudflare.com/ajax/libs/stockfish.js/10.0.2/stockfish.js')
       .then(response => response.text())
       .then(text => sendResponse({ code: text }))
       .catch(err => sendResponse({ error: err.toString() }));
-    return true; // Wajib: memberitahu Chrome bahwa respon akan dikirim secara asynchronous
+    return true;
   }
 });
